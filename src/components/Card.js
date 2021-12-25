4a38881aed05c8f2fb997eb7b5578c75374dbc1d
LAPTOP-E8U4FAPL\Koichi <ant19071916@gmail.com>
2021-12-25 23:45:59 +0900

    [add] added svg files

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,29 +1,28 @@
 import PropTypes from "prop-types";
-import pic from "../images/green-rect-filled.png";
 
-const Card = ({ fileName }) => {
-  // function importAll(r) {
-  //   let images = {};
-  //   r.keys().map((item, index) => {
-  //     images[item.replace("./", "")] = r(item);
-  //   });
-  //   return images;
-  // }
+const Card = ({ fileName, number }) => {
+  // dynamically importing images
+  function importAll(r) {
+    let images = {};
+    r.keys().map((item, index) => {
+      images[item.replace("./", "")] = r(item);
+    });
+    return images;
+  }
 
-  // const images = importAll(
-  //   require.context("../images", false, /\.(png|jpe?g|svg)$/)
-  // );
+  const images = importAll(
+    require.context("../images", false, /\.(png|jpe?g|svg)$/)
+  );
+
+  const onClick = () => {
+    console.log({ fileName });
+  };
 
   return (
-    <div
-      className="card"
-      onClick={() => {
-        console.log("clicked");
-      }}
-    >
-      <img src={pic} alt="" height="15px" width="50px"></img>
-      <img src={pic} alt="" height="15px" width="50px"></img>
-      <img src={pic} alt="" height="15px" width="50px"></img>
+    <div className="card" onClick={onClick}>
+      {[...Array(number)].map((e, i) => (
+        <img className="card-mark" src={images[fileName].default} alt="" />
+      ))}
     </div>
   );
 };
@@ -33,10 +32,11 @@ Card.defaultProps = {
 };
 
 Card.propTypes = {
-  Color: PropTypes.number,
-  Shape: PropTypes.number,
-  Pattern: PropTypes.number,
-  Number: PropTypes.number,
+  color: PropTypes.number,
+  shape: PropTypes.number,
+  pattern: PropTypes.number,
+  number: PropTypes.number,
+  fileName: PropTypes.string,
 };
 
 export default Card;
